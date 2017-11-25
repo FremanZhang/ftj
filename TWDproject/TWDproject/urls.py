8d5394b928cef7fb4fe5ebb156574c1d1c4d1d29
Freman Zhang <freman@yeah.net>
2017-11-25 15:03:58 +0800

    urlchain

--- TWDproject/TWDproject/urls.py
+++ TWDproject/TWDproject/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rango/', include('rango/index')),
+    url(r'^rango/', include('rango.urls')),
 ]
