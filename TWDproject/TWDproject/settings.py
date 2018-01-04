66ebc05b48b4d07b0959f87eb3ca381de5abda62
Freman Zhang <freman@yeah.net>
2018-01-04 16:12:01 +0800

    ‘template_setting’

--- TWDproject/TWDproject/settings.py
+++ TWDproject/TWDproject/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -52,7 +53,10 @@ MIDDLEWARE = [
 
 # Need to use absolute paths to the location of 'templates' directory
 TEMPLATE_DIRS = (
-    '/home/freman/lp/TWDproject/templates/rango',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 ROOT_URLCONF = 'TWDproject.urls'
