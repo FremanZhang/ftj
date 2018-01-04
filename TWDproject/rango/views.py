e5ca4e51d6e0168a0786d85e8ccf2d9d2b4e0b69
Freman Zhang <freman@yeah.net>
2018-01-04 16:32:22 +0800

    template_index

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(" Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
+    # return HttpResponse(" Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': 'I am bold front from the context.'}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse("This is about page. <br/> <a href='/rango/'>Index</a>")
