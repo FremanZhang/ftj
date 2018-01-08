cbf1d02920355fa49470c46e48026c9f597d1d10
Freman Zhang <freman@yeah.net>
2018-01-08 10:31:15 +0800

    v5.6

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -11,4 +11,8 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("This is about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict = {
+        'aboutmessage': 'Welcome for contact as below.'
+    }
+    return render(request, 'rango/about.html', context_dict)
+    # return HttpResponse("This is about page. <br/> <a href='/rango/'>Index</a>")
