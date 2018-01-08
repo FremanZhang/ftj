9e9c1f7cb0b9da45a059f4fac5f87fa6aa882143
Freman Zhang <freman@yeah.net>
2018-01-08 09:36:26 +0800

    media

--- TWDproject/TWDproject/urls.py
+++ TWDproject/TWDproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
-] + static(settings.MEDIA_URL, view=serve, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += patterns(
