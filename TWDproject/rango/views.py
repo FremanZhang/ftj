338340d322c44de3111dd65edfd62c08bca8272e
Freman Zhang <freman@yeah.net>
2018-01-09 17:10:40 +0800

    v7.2

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -3,12 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rango.models import Category, Page
 
 def index(request):
     # return HttpResponse(" Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
-    context_dict = {'boldmessage': 'I am bold front from the context.'}
+    context_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'top5_categories': context_list}
     return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
     context_dict = {
