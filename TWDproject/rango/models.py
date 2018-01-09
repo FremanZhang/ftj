7aec293b8def9991756f51312e6c773f537f4aa2
Freman Zhang <freman@yeah.net>
2018-01-09 17:45:38 +0800

    v7.3

--- TWDproject/rango/models.py
+++ TWDproject/rango/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.db.models import Model
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -10,6 +11,11 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
