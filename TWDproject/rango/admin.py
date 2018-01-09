8698c489ee59311d3522e62831890610f775cebc
Freman Zhang <freman@yeah.net>
2018-01-09 15:05:07 +0800

    v6.9

--- TWDproject/rango/admin.py
+++ TWDproject/rango/admin.py
@@ -5,5 +5,21 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 # Register your models here.
+'''
+# Preceding register
 admin.site.register(Category)
 admin.site.register(Page)
+'''
+
+
+# Admin regisyer decorator
+@admin.register(Category,Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'views', 'likes')
+    empty_value_display = '-'
+    
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title, 'category', 'url', 'views')
+    empty_value_display = '-'
