f9ed0b210c9c94361ea171aaff3929edf2088efb
Freman Zhang <freman@yeah.net>
2018-01-09 11:38:12 +0800

    v6.7

--- TWDproject/rango/population_rango.py
+++ TWDproject/rango/population_rango.py
@@ -60,7 +60,7 @@ def add_cat(name):
     return c
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
