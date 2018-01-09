6e0b7790486ab56d3433968e5f86dc67c29b41be
Freman Zhang <freman@yeah.net>
2018-01-09 13:46:20 +0800

    v6.9

--- TWDproject/rango/models.py
+++ TWDproject/rango/models.py
@@ -8,6 +8,8 @@ from django.db.models import Model
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=NOT_PROVIDED)
 
     def __unicode__(self):
         return self.name
