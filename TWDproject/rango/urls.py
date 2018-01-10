2655aadc6eb16a8ef4f9a3343dd7a451b18fd5f4
Freman Zhang <freman@yeah.net>
2018-01-10 17:26:24 +0800

    v7.3

--- TWDproject/rango/urls.py
+++ TWDproject/rango/urls.py
@@ -8,8 +8,9 @@ import views
 # )
 
 # The one in 1.11
+#### [\w\-]+ 指一个或多个[字母，数字，下划线或中横线]
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^category/(?P<category_slug_name>[\w\-]+/$)', views.catefory, name='category'), #[\w\-]+ 指一个或多个[字母，数字，下划线或中横线]
+    url(r'^category/(?P<category_slug_name>[\w\-]+/$)', views.catefory, name='category'),
 ]
