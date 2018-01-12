2ee0d387f348705441d997fe2e4f237311b44a2b
Freman Zhang <freman@yeah.net>
2018-01-12 17:04:01 +0800

    v8.2.1

--- TWDproject/rango/forms.py
+++ TWDproject/rango/forms.py
@@ -10,7 +10,7 @@ class CategoryForm(forms.ModelForm):
 
     class Meta:
         model = Category
-        fields = ('name')
+        fields = ('name',)
 
 
 class PageForm(forms.ModelForm):
@@ -20,5 +20,4 @@ class PageForm(forms.ModelForm):
 
     class Meta:
         model = Page
-        exclude = ('category')
-        
+        exclude = ('category',)
