e80fbc0022fcd7b651405e8edbbbd04e64486108
Freman Zhang <freman@yeah.net>
2018-01-12 17:51:56 +0800

    v8.2

--- TWDproject/rango/forms.py
+++ TWDproject/rango/forms.py
@@ -26,8 +26,8 @@ class PageForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        if url and not url.startswith('http://')
+        if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
-            
+
         return cleaned_data
