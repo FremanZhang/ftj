ccf54e5497b412150e4e5b46766eb9c6d622069b
Freman Zhang <freman@yeah.net>
2018-01-30 18:00:58 +0800

    v9.4

--- TWDproject/rango/admin.py
+++ TWDproject/rango/admin.py
@@ -4,6 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from rango.models import Category, Page, UserProfile
 
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+admin.site.register(User, UserAdmin)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'views', 'likes')
@@ -15,7 +21,7 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url', 'views')
     empty_value_display = '-'
 
-# Register your models here.
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
-admin.site.register(UserProfile)
+class UserAdmin(admin.UserAdmin):
+    list_display = ('username', 'email', 'password', 'date_joined', 'last_login')
+    empty_value_display = '-'
+
