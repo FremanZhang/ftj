6d29c2100cf140469e6d31a634f8d2f255ad11a8
Freman Zhang <freman@yeah.net>
2018-01-30 18:07:04 +0800

    v9.4

--- TWDproject/rango/admin.py
+++ TWDproject/rango/admin.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.models import User
 from rango.models import Category, Page, UserProfile
 
 
@@ -25,4 +24,3 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
-admin.site.register(User, UserAdmin)
