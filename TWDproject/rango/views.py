3611433cfaa2fab9d683bb4d8b006b21450aead8
Freman Zhang <freman@yeah.net>
2018-02-06 09:50:12 +0800

    v10.5

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -157,7 +157,7 @@ def user_login(request):
             print "Invalid login details: {0}, {1}".format(username, password)
             return HttpResponse('Invalid login details supplied.')
     else:
-        return render(request, '/rango/login.html', {})
+        return render(request, '/rango/about.html', {})
 
 
 @login_required
