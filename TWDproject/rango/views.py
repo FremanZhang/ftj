ea5b7b72735af241c21253cda84eefc5534d49ae
Freman Zhang <freman@yeah.net>
2018-02-26 17:33:16 +0800

    v11.6

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -56,7 +56,7 @@ def index(request):
     last_visit = request.session.get('last_visit')
     if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-        if (datetime.now() - last_visit_time).second > 0:
+        if (datetime.now() - last_visit_time).seconds > 0:
             visits = visits + 1
             reset_last_visit_time = True
     else:
