f39f0b8c0d52c2497cefaa1922982d0e761cd9d0
Freman Zhang <freman@yeah.net>
2018-02-26 15:56:02 +0800

    v11.5

--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -35,8 +35,8 @@ def index(request):
 
     if 'last_visit' in request.COOKIES:
         last_visit = request.COOKIES['last_visit']
-        last_visit_time = datetime.strptime(last_visit[:7], "%Y-%m-%d %H:%M:%S")
-        if (datetime.now() - last_visit_time).seconds > 1:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        if (datetime.now() - last_visit_time).seconds > 0:
             visits = visits + 1
             reset_last_visit_time = True
     else:
