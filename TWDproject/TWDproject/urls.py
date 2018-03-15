e076ce4515dd545890341a937f97404b1e60e1ad
Freman Zhang <freman@yeah.net>
2018-03-15 16:27:56 +0800

    14.2

--- TWDproject/TWDproject/urls.py
+++ TWDproject/TWDproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from rango import views
 from django.conf.urls.static import static
 from registration.backends.default.views import RegistrationView
 
