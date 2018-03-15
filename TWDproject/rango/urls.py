1b05eeab5b8f5eb62eec05ddab5fd8520d23ca50
Freman Zhang <freman@yeah.net>
2018-03-15 17:12:20 +0800

    14.2

--- TWDproject/rango/urls.py
+++ TWDproject/rango/urls.py
@@ -12,8 +12,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^category/$', views.index, name='category_list'),
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     # url(r'^register/$', views.register, name='register'),
