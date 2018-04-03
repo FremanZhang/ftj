5323ab90fdb17304148a64adecf7bce78060ba17
Freman Zhang <freman@yeah.net>
2018-04-03 17:40:03 +0800

    18.3

--- TWDproject/static/bootstrap/js/rango-jquery.js
+++ TWDproject/static/bootstrap/js/rango-jquery.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         $(this).css('color', 'red');
     },
     function(){
-        $(this).css('color', 'blue');
+        $(this).css('color', 'black');
     });
 
 });
