0089e307049683ad385312ff1e76e8be98703cbe
Freman Zhang <freman@yeah.net>
2018-04-03 18:06:08 +0800

    18.4

--- TWDproject/static/bootstrap/js/rango-jquery.js
+++ TWDproject/static/bootstrap/js/rango-jquery.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     //JQuery code to be added in here.
     $("#about-btn").click(function(event){
-        alert("You clicked the button using JQuery!")
+        // alert("You clicked the button using JQuery!")
+        msgstr = $("#msg").html()
+                msgstr = msgstr + "o"
+                $("#msg").html(msgstr)
     });
 
     $("p").hover(function(){
