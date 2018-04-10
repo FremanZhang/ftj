b989f5e24051805bca411ba3a83e17fc0d62bef2
Freman Zhang <freman@yeah.net>
2018-04-10 16:20:23 +0800

    19.2

--- TWDproject/static/bootstrap/js/rango-ajax.js
+++ TWDproject/static/bootstrap/js/rango-ajax.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     // JQuery code to be added in here.
-    $('#likes').click(function(){
+    $('#likes').click(function(event){
         var catid;
         catid = $(this).attr('data-catid');
+        alert(catid)
         $.get("/rango/like_category/?category_id=2", function(data){
             alert("数据：" + data + "\n状态：" + status);
             $('#like_count').html(data);
