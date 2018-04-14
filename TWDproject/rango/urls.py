afb0053ec190bb70fb657a68d2b9c116190833e7
Freman Zhang <freman@yeah.net>
2018-04-14 17:39:11 +0800

    19.3

--- TWDproject/rango/urls.py
+++ TWDproject/rango/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     # url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^goto/$', views.track_url, name='goto'),
     url(r'^like_category/$', views.like_category, name='like_category'),
+    url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
 ]
--- TWDproject/rango/views.py
+++ TWDproject/rango/views.py
@@ -266,3 +266,27 @@ def like_category(request):
     print likes
     return HttpResponse(likes)
 
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = get_object_or_404(Category, name__istartswith=starts_with)
+    
+    if cat_list and max_results > 0:
+        if cat_list.count() > max_results:
+            cat_list = cat_list[:max_results]
+    
+    return cat_list
+
+
+def suggest_category(request):
+    cat_list = []
+    starts_with = ''
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+
+    cat_list = get_category_list(8, starts_with)
+
+    return render(request, 'rango/cats/.html', {'cat_list': cat_list})
+
+    
